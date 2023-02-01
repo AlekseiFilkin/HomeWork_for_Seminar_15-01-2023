b1a12260da7d70abf854ae72065cebc6b606c0c6
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-01 12:20:23 +0700

    SquareOf2NumbersTask solved

--- SquareOf2NumbersTask/Program.cs
+++ SquareOf2NumbersTask/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine() ?? "");
+if (num1 == num2 * num2)
+    {
+        Console.Write($"Число {num1} является квадратом числа {num2}");
+    }
+    else if (num2 == num1 * num1)
+        {
+            Console.Write($"Число {num2} является квадратом числа {num1}");
+        }
+            else if (num1 != num2 * num2 && num2 != num1 * num1)
+                {
+                    Console.Write("Числа не являются квадратами друг друга");
+                }
